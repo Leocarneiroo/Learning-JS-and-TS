d12911c19ba92cf1e9193526d9b890b48146d49d
Leonardo Carneiro <leocarneiroo@gmail.com>
2022-08-25 21:25:06 -0300

    condições

---
+++ Mód 2/aula 37/index.js
@@ -0,0 +1,10 @@
+
+const nome = 'Luiz Otavio';
+
+/*for (let i in nome) {
+    console,log(nome[i]);
+}*/
+
+for ( let valor of nome) {
+    console.log(valor); // neste caso volta o valor e não o indice.
+}
